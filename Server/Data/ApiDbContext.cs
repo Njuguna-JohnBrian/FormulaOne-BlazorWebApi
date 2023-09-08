bb834751ca2fcf280f652f6d3cb86752f67e5c4b
John Brian <njugunajb96@gmail.com>
2023-09-08 20:41:50 +0300

    db context

---
+++ Server/Data/ApiDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FormulaOne.Server.Data;
+
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+    {
+    }
+}
