4dec8479915d6fa5eb16d224a267cf2c02f3f406
John Brian <njugunajb96@gmail.com>
2023-09-10 08:46:53 +0300

    create driver model

---
+++ Shared/Models/Driver.cs
@@ -0,0 +1,9 @@
+﻿namespace FormulaOne.Shared.Models;
+
+public class Driver
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string RacingNb { get; set; } = string.Empty;
+    public string Team { get; set; } = string.Empty;
+}
