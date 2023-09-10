720c2320604b955943e5b0becb8201e476632602
John Brian <njugunajb96@gmail.com>
2023-09-10 12:03:41 +0300

    create driver service interface

---
+++ Client/Services/IDriverService.cs
@@ -0,0 +1,17 @@
+﻿using FormulaOne.Client.Pages;
+using FormulaOne.Shared.Models;
+
+namespace FormulaOne.Client.Services;
+
+public interface IDriverService
+{
+    Task<IEnumerable<Driver>?> All();
+    
+    Task<Driver?> GetDriver(int id);
+    
+    Task<bool?> AddDriver(Driver driver);
+    
+    Task<bool> Update(Driver driver);
+    
+    Task<bool> Delete(int id);
+}
