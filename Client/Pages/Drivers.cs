c92f87e72eba79c4b67089d7ddcb4e0b293c6896
John Brian <njugunajb96@gmail.com>
2023-09-10 12:02:47 +0300

    create drivers page code logic

---
+++ Client/Pages/Drivers.cs
@@ -0,0 +1,24 @@
+﻿using FormulaOne.Client.Services;
+using FormulaOne.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace FormulaOne.Client.Pages;
+
+public partial class Drivers
+{
+    [Inject] private IDriverService driverService { get; set; }
+
+    public IEnumerable<Driver> _drivers { get; set; } = new List<Driver>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        var apiDrivers = await driverService.All();
+
+        if (apiDrivers != null)
+        {
+            IEnumerable<Driver> enumerable = apiDrivers as Driver[] ?? apiDrivers.ToArray();
+            if (enumerable.Any())
+                _drivers = enumerable;
+        }
+    }
+}
